c56db840c9003c6a48aeea32f6f052973a71cc13
Jafer Haider <itsjafer@gmail.com>
2022-07-29 22:49:09 +0000

    Change back to headful

--- scrapers.js
+++ scrapers.js
@@ -76,7 +76,7 @@ export const united = async (req, res) => {
   let browser = null;
   let flights = []
   try {
-    browser = await playwright.launchChromium({headless: true});
+    browser = await playwright.launchChromium();
     const context = await browser.newContext({
         userAgent:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
         viewport: { 'width': 1920, 'height': 1080 }
