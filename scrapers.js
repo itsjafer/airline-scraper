3b665e6a47f4c80e0ef5f4600c4d648415c612b2
Jafer Haider <itsjafer@gmail.com>
2022-07-29 23:50:07 +0000

    Add CORS

--- scrapers.js
+++ scrapers.js
@@ -105,6 +105,15 @@ export const unitedFunc = async (origin, destination, date) => {
 };
 
 export const united = async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    return res.status(204).send('');
+  }
   if (req.method !== 'POST') {
     // Return a "method not allowed" error
     return res.status(405).end();
